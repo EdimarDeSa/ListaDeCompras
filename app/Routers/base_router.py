441be87eb71d0ed4b242efb4d7a7c243fff83345
Edimar de Sá <edimar.sa@efscode.com.br>
2024-08-29 18:44:11 -0300

    Implementação do base router

--- app/Routers/base_router.py
+++ app/Routers/base_router.py
@@ -9,17 +9,34 @@ from app.Schemas.responses.base_response import BaseResponse, BaseContent
 from app.Services.user_service import BaseService
 
 
-class BaseRouter(ABC, APIRouter):
+class BaseRoutes(ABC):
+    _router: APIRouter = None
+
     @abstractmethod
-    def create_service(self) -> type[BaseService]:
+    def _create_service(self) -> type[BaseService]:
         pass
 
     @staticmethod
     def return_exception(e: Exception) -> BaseResponse:
-
         if isinstance(e, BaseInternalException):
             content = BaseContent(rc=e.rc, data=e.message)
             return BaseResponse(status_code=e.status_code, content=content)
 
         content = BaseContent(rc=ResponseCode.UNKNOWN_ERROR)
         return BaseResponse(status_code=st.HTTP_500_INTERNAL_SERVER_ERROR, content=content)
+
+    @staticmethod
+    def create_api_router(**kwargs) -> APIRouter:
+        return APIRouter(**kwargs)
+
+    @property
+    def router(self):
+        if self._router is None:
+            raise Exception("Router not created")
+        return self._router
+
+    @router.setter
+    def router(self, value: APIRouter) -> None:
+        if self._router is not None:
+            raise Exception("Router already created")
+        self._router = value
