8053262114041ae82732869b9573164b3bb862d1
Edimar de Sá <edimar.sa@efscode.com.br>
2024-09-09 09:34:11 -0300

    Melhorando estrutura de configuração da API

--- app/api_configs.py
+++ app/api_configs.py
@@ -2,8 +2,10 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from app.InternalResponse.internal_errors import InternalErrors
 from app.Middlewares.process_time import add_process_time_header
 from app.Routers.auth_router import AuthRoutes
 from app.Routers.base_router import BaseRoutes
@@ -14,43 +16,63 @@ from app.Routers.user_router import UserRoutes
 from app.Routers.utils_router import UtilsRoutes
 
 __all__ = [
-    "routers",
+    "TITLE",
+    "DESCRIPTION",
+    "DOCS_URL",
     "DEBUG_MODE",
+    "CONTACT",
+    "SWAGGER_UI_PARAMETERS",
+    "routers",
     "middlewares",
+    "register_middlewares",
+    "register_routes",
 ]
 
+from app.Utils.internal_types import METHODS
 
-routers: list[type[BaseRoutes]] = [
-    AuthRoutes,
-    UserRoutes,
-    UtilsRoutes,
-    UnityTypeRoutes,
-    DefaultCategoryRoutes,
-    DefaultProductsRoutes,
-]
+### APP CONFIG ###
+
+TITLE = "Mundo de compras"
 
 
-origins: list[str] = ["*"]
+DESCRIPTION = "Aplicação RestFull de lista de compras"
+
+DOCS_URL = "/docs"
+
+CONTACT = {
+    "name": "Edimar de Sá",
+    "email": "edimar.sa@efscode.com",
+    "url": "https://efscode.com",
+}
+
+SWAGGER_UI_PARAMETERS = {
+    "docExpansion": "none",
+    "deepLinking": False,
+    "persistAuthorization": True,
+    "displayOperationId": False,
+    "displayRequestDuration": True,
+    "defaultModelsExpandDepth": 0,
+    "filter": True,
+    "operationsSorter": "method",
+    "requestSnippets": [
+        {"lang": "curl", "label": "cURL"},
+        {"lang": "python", "label": "Python Requests"},
+    ],
+    "requestTimeout": 5000,
+    "showExtensions": True,
+    "showCommonExtensions": True,
+    "syntaxHighlight": True,
+    "supportedSubmitMethods": METHODS,
+    "tryItOutEnabled": False,
+    "theme": "flattop",
+}
 
 
-middlewares = [
-    {
-        "middleware_class": CORSMiddleware,
-        "options": {
-            "allow_origins": origins,
-            "allow_credentials": True,
-            "allow_methods": ["*"],
-            "allow_headers": ["*"],
-        },
-    },
-    {"middleware_class": add_process_time_header, "options": {"http": True}},
-]
+### LOGGER ###
+logger = logging.getLogger(__name__)
 
 
 DEBUG_MODE: bool = bool(int(os.getenv("DEBUG_MODE", "0")))
-
-
-logger = logging.getLogger(__name__)
 
 
 if DEBUG_MODE:
@@ -71,3 +93,57 @@ if DEBUG_MODE:
         logger.addHandler(rotating_handler)
 
     logger.info("Logger activated!")
+
+
+### ORIGINS ###
+origins: list[str] = ["*"]
+
+
+### MIDDLEWARES ###
+middlewares = [
+    {
+        "middleware_class": CORSMiddleware,
+        "options": {
+            "allow_origins": origins,
+            "allow_credentials": True,
+            "allow_methods": ["*"],
+            "allow_headers": ["*"],
+        },
+    },
+    {"middleware_class": add_process_time_header, "options": {"http": True}},
+]
+
+
+### REGISTER MIDDLEWARES ###
+def register_middlewares(app: FastAPI) -> None:
+    for middleware in middlewares:
+        logger.debug(f"Iniciando - {middleware['middleware_class'].__name__}")
+
+        if "http" in middleware.get("options", False):
+            app.middleware("http")(middleware["middleware_class"])
+            return
+
+        app.add_middleware(middleware["middleware_class"], **middleware["options"])
+
+
+### ROUTERS ###
+routers: list[type[BaseRoutes]] = [
+    AuthRoutes,
+    UserRoutes,
+    UtilsRoutes,
+    UnityTypeRoutes,
+    DefaultCategoryRoutes,
+    DefaultProductsRoutes,
+]
+
+
+### REGISTER ROUTERS ###
+def register_routes(app: FastAPI) -> None:
+    for router in routers:
+        r = router()
+
+        if not isinstance(r, BaseRoutes):
+            raise Exception([InternalErrors.INTERNAL_SERVER_ERROR_500, "Roteador inválido"])
+
+        logger.debug(f"Starting - {r.__class__.__name__}")
+        app.include_router(r.api_router)
