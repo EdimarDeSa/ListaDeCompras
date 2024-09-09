32da7bb39e5e18af2f69ef780df1b31b62cf3e88
Edimar de Sá <edimar.sa@efscode.com.br>
2024-09-09 09:35:17 -0300

    Estruturando configuração da API

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
 from dotenv import load_dotenv
 
-from app.InternalResponse.internal_errors import InternalErrors
-from app.Routers.base_router import BaseRoutes
-
 load_dotenv()
-
-import logging
 
 from fastapi import FastAPI
 
 from app.api_configs import *
 
-logger = logging.getLogger(__name__)
-
 
 app = FastAPI(
-    title="Mundo de compras",
+    title=TITLE,
+    description=DESCRIPTION,
     debug=DEBUG_MODE,
-    docs_url="/docs",
-    contact={"name": "Edimar de Sá", "email": "edimar.sa@efscode.com"},
+    docs_url=DOCS_URL,
+    contact=CONTACT,
+    swagger_ui_parameters=SWAGGER_UI_PARAMETERS,
 )
 
+register_middlewares(app)
 
-# Registra todos os middlewares listados
-for middleware in middlewares:
-    logger.debug(f"Iniciando - {middleware['middleware_class'].__name__}")
-    if "http" in middleware.get("options", False):
-        app.middleware("http")(middleware["middleware_class"])
-    else:
-        app.add_middleware(middleware["middleware_class"], **middleware["options"])
-
-
-# Registra todos os roteadores listados
-for router in routers:
-    r = router()
-
-    if not isinstance(r, BaseRoutes):
-        raise Exception([InternalErrors.INTERNAL_SERVER_ERROR_500, "Roteador inválido"])
-
-    logger.debug(f"Starting - {r.__class__.__name__}")
-    app.include_router(r.api_router)
+register_routes(app)
